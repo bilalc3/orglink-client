c9f0d9df8a914bc44088c7c40efec4380e856246
awaischoudhary <awaischoudharyy@gmail.com>
2023-07-09 01:12:07 -0400

    Homepage title added

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import './App.css';
 import Navbar from './Navbar.js';
 import Footer from './Footer';
-import logo from './donate.webp';
+import logo from './donate1.png';
 
 function App() {
   return (
@@ -13,10 +13,15 @@ function App() {
           <img className="logo" src={logo} alt="" />
         </div>
         <div className = "right-container"> 
-            OrgLink
-            <button className = "button">
-              Start Talking Today!
-            </button>
+          <h1 className = "words">
+            Connect. Educate. Spread.
+          </h1>
+          <h3 className = "homepage-message">
+            Saving Lives Together: Join the Organ Donation Movement and Make a Difference Today! Be a Hero, Donate Your Organs
+          </h3>
+          <button className = "button">
+            Start Talking Today!
+          </button>
         </div>
       </div>
       <Footer />
